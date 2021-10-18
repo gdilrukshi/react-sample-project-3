ea7e822b4914841a9d8836d0a17757c766c97a13
gdilrukshi <gimhanidilrukshi1996@gmail.com>
2021-10-18 12:11:04 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+import Navimg from "../../images/1.png";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img className="navgimg" src={Navimg} alt='logo' />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/services' activeStyle>
+            Services
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Buy Now
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/about.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import Regimg from "../images/3.png";
+
+const About = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+
+      <h1 class="center">About</h1>
+
+      {/* <div class="row">
+        <h1 class="center">About</h1>
+
+        <img className="regimg center" src={Regimg} alt="icon" />
+      </div> */}
+
+
+
+    </div>
+  );
+};
+
+export default About;
